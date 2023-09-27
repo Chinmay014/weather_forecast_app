ac18e957dede8452e1b4932bed9fa71727415144
Chinmay014 <chinmays117@gmail.com>
2023-09-27 18:22:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A weather forecast app
+
+This app lets the user see the weather for a particular place for next few days
---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Weather forecast for the next days")
+# st.write("Place:")
+
+place = st.text_input("Place:")
+
+# st.text("Forecast days")
+days = st.slider("Forecast days",min_value=1,max_value=5,help="Select the number of days in future")
+
+option = st.selectbox("Select data to view",
+                      ("Temperature","Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
